Bug fix for escape sequence + Enabling scaling loading

src/pyigor/pyigor.py
@@ -5,10 +5,12 @@ import queue
 import uuid
 import subprocess
 import re, ast, json, glob, platform
+from typing import List
 
 import flask
 from flask import Flask
 import h5py
+import numpy as np
 
 my_platform = "mac" if platform.platform(terse=True).startswith("macOS") else ("windows" if platform.platform(terse=True).startswith("Windows") else None)
 
@@ -19,8 +21,8 @@ def alphanumeric_sort(l):
 
 ##### OS dependent codes #####
 def find_executable_path():
-    exe_path_dict = {   "mac": "/Applications/Igor Pro * Folder/Igor64.app/Contents/MacOS/Igor64",
-                        "windows": "C:\Program Files\WaveMetrics\Igor Pro * Folder\IgorBinaries_x64\Igor64.exe"}
+    exe_path_dict = {"mac": os.path.join("/Applications", "Igor Pro * Folder", "Igor64.app", "Contents", "MacOS", "Igor64"),
+                    "windows": os.path.join("C:", "Program Files", "WaveMetrics", "Igor Pro * Folder", "IgorBinaries_x64", "Igor64.exe")}
 
     path_candidates = glob.glob(exe_path_dict[my_platform])
     assert len(path_candidates) > 0, "Cannot find Igor Pro"
@@ -45,8 +47,8 @@ class Connection:
     ### security_hole options makes it possible to execute any Python code by HTTP requests. Do not use unless you are sure of it.
     def __init__(self, port=15556, security_hole=False, timeout=3):
         self._app = Flask(__name__)
-        self._task_queue = queue.Queue(maxsize=1)
-        self._queue = queue.Queue(maxsize=1)
+        self._task_queue = queue.Queue(maxsize=1) # set of (command, uid)
+        self._queue = queue.Queue(maxsize=1) # set of (status, uid, data_dict or None)
         self._port = port
         self._registered_functions = {"get": self.get, "put": self.put, "print": print}
         self._basepath = os.getcwd()
@@ -59,7 +61,7 @@ class Connection:
 
     def reset(self):
         try:
-            self._queue.put_nowait(("error", 0))
+            self._queue.put_nowait(("error", 0, None))
         except:
             pass
         try:
@@ -131,9 +133,9 @@ class Connection:
                 result = self._process_get(uid)
                 self._queue.put_nowait(result)
             if msg == "put":
-                self._queue.put_nowait(("ok", uid))
+                self._queue.put_nowait(("ok", uid, None))
             if msg == "error":
-                self._queue.put_nowait(("error", uid))
+                self._queue.put_nowait(("error", uid, None))
             return "<p>Bridging Igor and Python</p>"
 
         @self._app.route("/call/<string:commands>")
@@ -160,6 +162,13 @@ class Connection:
     def _process_get(self, uid):
         with h5py.File(self._temp_path(), mode="r") as f:
             result_dict = {"array": f[uid][...]}
+            attrs = f[uid].attrs
+            if "IGORWaveScaling" in attrs:
+                result_dict["offsets"] = list(attrs["IGORWaveScaling"][1:, 1])
+                result_dict["deltas"] = list(attrs["IGORWaveScaling"][1:, 0])
+            else: # IGORWaveScaling is omitted for default parameters
+                result_dict["offsets"] = [0.0] * len(result_dict["array"].shape)
+                result_dict["deltas"] = [1.0] * len(result_dict["array"].shape)
         return ("ok", uid, result_dict)
     
     def _temp_path(self, for_igor=False):
@@ -188,17 +197,35 @@ class Connection:
         return wrapper
 
 class Wave:
-    def __init__(self, array):
+    def __init__(self, array, offsets: List[float], deltas: List[float], units: str=""):
+        if isinstance(array, list):
+            array = np.array(list)
         self.array = array
-        self.offset = [0]
-        self.delta = [1]
-        self.units = [None]
+        self.offsets = offsets
+        self.deltas = deltas
+        self.units = units
     
     @classmethod
     def from_dict(cls, d):
-        wave = Wave(d["array"])
+        wave = Wave(**d)
         return wave
+
+    @property
+    def shape(self):
+        return self.array.shape
+
+    @property
+    def numpnts(self):
+        return self.array.size
     
-    def __str__(self):
-        return f"<Wave shape:{self.array.shape}, data_type:{self.array.dtype}>"
+    @property
+    def deltax(self):
+        return self.deltas[0]
+
+    @property
+    def leftx(self):
+        return self.offsets[0]
+
+    def __repr__(self):
+        return f"<pyigor.Wave shape: {self.shape}, data_type: {self.array.dtype}>"
 